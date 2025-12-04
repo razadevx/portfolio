5ec6dd6506cdb5983cbebd8bfe3ae81f1f5b15fc
Ahmad Raza <razadeveloperx@gmail.com>
2025-12-04 06:38:21 +0500

    Fix: Added basename to BrowserRouter for GH Pages 404 fix

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,8 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      {/* 🎯 FIX APPLIED HERE: Added the basename prop */}
+      <BrowserRouter basename="/portfolio">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
